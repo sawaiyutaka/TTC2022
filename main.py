309a8f5ee52d12aac1bb00baf8b81ca01a1bcfc2
sawaiyutaka <yusawai.tky@gmail.com>
2022-10-21 16:19:35 +0900

    read all data in 1st wave

--- main.py
+++ main.py
@@ -22,10 +22,14 @@ list = []
 
 # プログラム4｜プログラム2で取得したエクセルを一つずつpandasデータとして取得
 for file in files:
-    list.append(pd.read_excel(file))
+    d = pd.read_excel(file)
+    print(d)
+    d = d.set_index("SAMPLENUMBER")
+    print(d)
+    list.append(d)
 
 # プログラム5｜listに格納されたエクセルファイルをpandasとして結合
-df = pd.concat(list)
+df = pd.concat(list, axis=1, join='inner')
 
 # プログラム6｜エクセルファイルを書き出す
 df.to_csv("/Volumes/Pegasus32R8/TTC/2022csv/TTC2022_1st_all.csv")
