d9b637e1dae5f2689444c5b99770470d57174212
sawaiyutaka <113234392+sawaiyutaka@users.noreply.github.com>
2023-01-07 00:54:02 +0900

    Add files via upload

---
+++ exploratory/8_binary.py
@@ -0,0 +1,92 @@
+import sys
+import sklearn.neighbors._base
+
+sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
+import pandas as pd
+import codecs
+from missingpy import MissForest
+
+df0 = pd.read_table("columns_NAN_under_92.csv", delimiter=",")
+df0 = df0.set_index("SAMPLENUMBER")
+print(df0)
+
+df0 = df0[df0['OCS_0or1'] == 1]
+print(df0)
+
+# 第２期のAQ素点からAQを計算
+# AQの合計点を作成
+df_AQ = df0[["BB123", "BB124", "BB125", "BB126", "BB127", "BB128", "BB129", "BB130", "BB131", "BB132"]].copy()
+print(df_AQ)
+
+for i in ["BB123", "BB124", "BB128", "BB129", "BB130", "BB131"]:
+    df_AQ = df_AQ.replace({i: {1: 0, 2: 0, 3: 1, 4: 1, 5: 0}})
+    print(df_AQ)
+
+for i in ["BB125", "BB126", "BB127", "BB132"]:
+    df_AQ = df_AQ.replace({i: {1: 1, 2: 1, 3: 0, 4: 0, 5: 0}})
+    print(df_AQ)
+
+df0["AQ_sum"] = df_AQ.sum(axis=1)
+print("第2回AQ合計\n", df0["AQ_sum"])
+
+# PLEで全項目回答なしは除外
+df0 = df0.dropna(subset=["CD57_1", "CD58_1", "CD59_1", "CD60_1", "CD61_1", "CD62_1", "CD63_1", "CD64_1", "CD65_1"],
+                 how='all')
+print(df0)
+
+# 第4期のPLEを除外
+df_3rd = df0.drop(["DD64_1", "DD65_1", "DD66_1", "DD67_1", "DD68_1", "DD69_1", "DD70_1", "DD71_1", "DD72_1"], axis=1)
+
+# 第２期のAQ素点を除外
+df_3rd = df_3rd.drop(["BB123", "BB124", "BB125", "BB126", "BB127", "BB128", "BB129", "BB130", "BB131", "BB132"], axis=1)
+
+# Make an instance and perform the imputation
+imputer = MissForest()
+df_imputed = imputer.fit_transform(df_3rd)
+print("df_imputed\n", df_imputed)
+df_3rd[df_3rd.columns.values] = df_imputed
+df_3rd = df_3rd.round().astype(int)  # 各列を整数に丸める（身長、体重も丸め）
+df_3rd.to_csv("df_3rd_imp8.csv")
+print("set_indexは？\n", df_3rd)
+
+"""
+# 1つでも「2回以上あった」があった人をPLEありとする
+df_ple_3rd = df[
+    (df['CD57_1'] == 4) | (df['CD58_1'] == 4) | (df['CD59_1'] == 4) | (df['CD60_1'] == 4) | (df['CD61_1'] == 4)
+    | (df['CD62_1'] == 4) | (df['CD63_1'] == 4) | (df['CD64_1'] == 4) | (df['CD65_1'] == 4)].copy()
+print(df_ple_3rd)
+df_ple_3rd["group_3rd"] = 1
+
+# 全ての項目に回答があって、「1回あった」までの人はPLEなしとする(300427は除外される)
+df_non_3rd = df[
+    (df['CD57_1'] < 4) & (df['CD58_1'] < 4) & (df['CD59_1'] < 4) & (df['CD60_1'] < 4) & (df['CD61_1'] < 4)
+    & (df['CD62_1'] < 4) & (df['CD63_1'] < 4) & (df['CD64_1'] < 4) & (df['CD65_1'] < 4)].copy()
+print(df_non_3rd)
+df_non_3rd["group_3rd"] = 0
+
+df_3rd = pd.concat([df_ple_3rd, df_non_3rd])
+print(df_3rd)
+"""
+# PLEの合計点を作成(第3期)
+df_Y = df_3rd[["CD57_1", "CD58_1", "CD59_1", "CD60_1", "CD61_1", "CD62_1", "CD63_1", "CD64_1", "CD65_1"]].copy()
+print("df_Y\n", df_Y)
+df_3rd["PLE_sum_3rd"] = df_Y.sum(axis=1)
+print("第3回PLE合計\n", df_3rd["PLE_sum_3rd"])
+
+# 第3期のPLEを除外
+df_3rd = df_3rd.drop(
+    ["CD57_1", "CD58_1", "CD59_1", "CD60_1", "CD61_1", "CD62_1", "CD63_1", "CD64_1", "CD65_1"],
+    axis=1)
+
+print(df_3rd)
+
+"""
+# 第1期の強迫、PLEを除外
+df_3rd = df_3rd.drop(["AB71", "AB87", "AB88", "AB104", "AB114", "AB126", "AB127", "AB145"], axis=1)
+df_3rd = df_3rd.drop(["AD57", "AD58", "AD59", "AD60", "AD61", "AD62"], axis=1)
+"""
+
+# df_3rd = df_3rd.loc[:, ~df_3rd.columns.duplicated()]
+# print("重複を削除\n", df_3rd)
+
+df_3rd.to_csv("df_3rd.csv")
---
+++ exploratory/9_boruta.py
@@ -0,0 +1,80 @@
+import sys
+import sklearn.neighbors._base
+
+sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
+import pandas as pd
+import numpy as np
+import codecs
+import japanize_matplotlib
+import matplotlib.pyplot as plt
+from missingpy import MissForest
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from boruta import BorutaPy
+import shap
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from multiprocessing import cpu_count
+
+df = pd.read_table("df_3rd.csv", delimiter=",")
+df = df.set_index("SAMPLENUMBER")
+print(df)
+
+
+# 特徴量 X、アウトカム y、割り当て変数 T
+y = df["PLE_sum_3rd"]
+X = df.drop(["PLE_sum_3rd"], axis=1)
+print(X.head())
+print(y)
+Y_train, Y_test, X_train, X_test = train_test_split(y, X, test_size=.2)
+
+# sklearnの機械学習モデル（ランダムフォレスト）のインスタンスを作成する
+# 教師データと教師ラベルを使い、fitメソッドでモデルを学習
+model = RandomForestRegressor(max_depth=None,
+                              max_features='sqrt',
+                              # X_train.shape[1],  # The number of features to consider when looking for the best split
+                              # 'sqrt'も可能
+                              min_samples_split=5,
+                              min_samples_leaf=1,
+                              n_estimators=5000,
+                              n_jobs=int(cpu_count() / 2),
+                              random_state=42)
+model.fit(X_train, Y_train)
+
+# 学習済みモデルの評価
+predicted_Y_val = model.predict(X_test)
+print("model_score: ", model.score(X_test, Y_test))
+
+# Borutaを実行
+rf = RandomForestRegressor(n_jobs=int(cpu_count() / 2), max_depth=7)
+feat_selector = BorutaPy(rf, n_estimators='auto', two_step=False, verbose=2, random_state=42)
+feat_selector.fit(X_train.values, Y_train.values)
+print(X_train.columns[feat_selector.support_])
+
+# 選択したFeatureを取り出し
+X_train_selected = X_train.iloc[:, feat_selector.support_]
+X_val_selected = X_test.iloc[:, feat_selector.support_]
+print(X_val_selected.head())
+
+# 選択したFeatureで学習
+rf2 = RandomForestRegressor(max_depth=None,
+                            max_features='auto',
+                            # X_train.shape[1],  # The number of features to consider when looking for the best split
+                            # 'sqrt'も可能
+                            min_samples_split=5,
+                            min_samples_leaf=1,
+                            n_estimators=5000,
+                            n_jobs=int(cpu_count() / 2),
+                            random_state=42)
+rf2.fit(X_train_selected.values, Y_train.values)
+
+predicted_Y_val_selected = rf2.predict(X_val_selected.values)
+print("model_score_2: ", rf2.score(X_val_selected, Y_test))
+
+# shap valueで評価（時間がかかる）
+# Fits the explainer
+explainer = shap.Explainer(rf2.predict, X_val_selected)
+# Calculates the SHAP values - It takes some time
+shap_values = explainer(X_val_selected)
+
+shap.plots.bar(shap_values, max_display=len(X_val_selected.columns))
+shap.summary_plot(shap_values, max_display=len(X_val_selected.columns))
